ebcbe72ae6bd69ed846ee541c025fb1d33b8e025
oKatanaaa <whitemarsstudios@gmail.com>
2018-11-05 15:42:04 +0400

    Created Drifter scene with setted up sprites

---
+++ addition/directions.gd
@@ -0,0 +1,7 @@
+extends Node
+
+const CENTER = Vector2(0, 0)
+const LEFT = Vector2(-1,0)
+const RIGHT = Vector2(1,0)
+const UP = Vector2(0,-1)
+const DOWN  = Vector2(0,1)
---
+++ scenes/drifter/drifter.gd
@@ -0,0 +1,50 @@
+extends KinematicBody2D
+
+const SPEED = 6000
+
+var moveDir = Dir.CENTER
+var spriteDir = "Down"
+
+func _physics_process(delta):
+	controls_loop()
+	movement_cycle(delta)
+	spriteDir_loop()
+	animation_cycle()
+	
+func movement_cycle(delta):
+	var motion = moveDir.normalized() * SPEED * delta
+	move_and_slide(motion)
+	
+func animation_cycle():
+	match moveDir:
+		Dir.CENTER:
+			switch_anim("Idle")
+		_:
+			switch_anim("Run")
+	
+func spriteDir_loop():
+	match moveDir:
+		Dir.LEFT:
+			spriteDir = "Left"
+		Dir.RIGHT:
+			spriteDir = "Right"
+		Dir.UP:
+			spriteDir = "Up"
+		Dir.DOWN:
+			spriteDir = "Down"
+	
+func switch_anim(animation):
+	var newAnim = str(animation, spriteDir)
+	if $anim.current_animation != newAnim:
+		$anim.play(newAnim)
+			
+	
+func controls_loop():
+	var LEFT = Input.is_action_pressed("ui_left")
+	var RIGHT = Input.is_action_pressed("ui_right")
+	var UP = Input.is_action_pressed("ui_up")
+	var DOWN = Input.is_action_pressed("ui_down")
+	
+	moveDir.x = -int(LEFT) + int(RIGHT)
+	moveDir.y = -int(UP) + int(DOWN)
+	
