4aa05484df748ea26c49003a76380af9fb137390
oKatanaaa <whitemarsstudios@gmail.com>
2018-11-14 23:36:22 +0400

    Deleted script file on  node.

--- scenes/drifter/anim.gd
+++
@@ -1,9 +0,0 @@
-extends AnimationPlayer
-
-const DEFAULT_SPEED = 1.0
-const AttackRightSpeed = 1.4
-func _process(delta):
-	if current_animation == "AttackRight":
-		playback_speed = AttackRightSpeed
-	else:
-		playback_speed = DEFAULT_SPEED
